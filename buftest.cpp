fe1da022b0b8f3779e3b114ab46bd6dc9f97c43c
Ivan Parfonov <2astloman@gmail.com>
2023-07-22 21:35:35 +0300

    Task solution
    Add: task realisation and simple tests for it
    
    Signed-off-by: Ivan Parfonov <2astloman@gmail.com>

--- buftest.cpp
+++ buftest.cpp
@@ -1,10 +1,13 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+using namespace std;
+
 #define uint8_t unsigned char
 
 // дан циклический (кольцевой) буфер и некоторые функции работы с ним
-#define BUFFER_SIZE 512
+#define BUFFER_SIZE 8
 
 #if (BUFFER_SIZE & (BUFFER_SIZE - 1)) != 0
   #error "Incorrect buffer size"
@@ -15,6 +18,22 @@ typedef struct {
   size_t last;
   uint8_t data[BUFFER_SIZE];
 } CircularBuffer;
+
+// ***********************************************************
+// *** Local functions declaration
+// ***********************************************************
+size_t GetDataCount(CircularBuffer* pBuf);
+
+// test func BufMoveSlow()
+void TestCase_01(CircularBuffer* pBufA, CircularBuffer* pBufB);
+
+// test func BufMoveFast()
+void TestCase_02(CircularBuffer* pBufA, CircularBuffer* pBufB);
+void TestCase_03(CircularBuffer* pBufA, CircularBuffer* pBufB);
+void TestCase_04(CircularBuffer* pBufA, CircularBuffer* pBufB);
+void TestCase_05(CircularBuffer* pBufA, CircularBuffer* pBufB);
+void TestCase_06(CircularBuffer* pBufA, CircularBuffer* pBufB);
+// ***********************************************************
 
 // ClearBuf очищает буфер (может также использоваться для инициализации структуры CircularBuffer)
 void ClearBuf(CircularBuffer* pBuf)
@@ -48,21 +67,29 @@ bool WriteByte(CircularBuffer* pBuf, uint8_t value)
 // пустым являтся буфер в котором нет данных для чтения.
 bool IsEmpty(CircularBuffer* pBuf)
 {
-  // TODO: напишите код этой фукнции
+ // TODO: напишите код этой фукнции
+ return (GetDataCount(pBuf) > 0 ? false : true);
 }
-
 
 // функция IsFull возвращает true если буфер полон, иначе false
 // попытка писать в полный буфер всегда будет завершаться неудачей.
 bool IsFull(CircularBuffer* pBuf)
 {
   // TODO: напишите код этой фукнции
+  return (GetDataCount(pBuf) < (BUFFER_SIZE - 1) ? false : true);
 }
 
 // что возвращает функция GetSomething? переименуйте ее, чтобы название соответствоало возвращаемому значению 
-size_t GetSomething(CircularBuffer* pBuf)
+size_t GetDataCount(CircularBuffer* pBuf)
 {
   return (pBuf->last - pBuf->first) & (BUFFER_SIZE - 1);
+}
+
+// Get count of free bytes in selected buffer
+//  but not used yet
+size_t GetEmptyCount(CircularBuffer* pBuf)
+{
+  return (BUFFER_SIZE - GetDataCount(pBuf));
 }
 
 // нам нужна фукнция для перемещения данных из одного циклического буфера в другой
@@ -88,6 +115,16 @@ size_t BufMoveSlow(CircularBuffer* pDest, CircularBuffer* pSource)
     result++;
   return result;
 }
+// @Ivan Parfonov
+// Відповідь про функцію BufMoveSlow() :
+// - переміщює байти з одного буфера в інший (але є нюанс, про це далі).
+// - робить перевірку що буфери pDest і pSource є різними (мають різні адреси).
+// - вертає кількість скопійований байт.
+// - не відповідає умові тех-завдання, при якій має залишати в буфері pSource всі байти, що не скопіювались в pDest
+//    якщо pDest вже повний. Це пов'язано з тим, що функція ReadByte() читаючі перший байт "видаляє" його з буфера
+//    збільшивши поле first на одиницю, тим самим переміщює початок буфера на наступний байт.
+//    Цей еффект можна побачить запустивши функцію TestCase_01(), в якості доказового приклада.
+
 
 // напишите свой вариант функции перемещения данных
 size_t BufMoveFast(CircularBuffer* pDest, CircularBuffer* pSource)
@@ -97,7 +134,41 @@ size_t BufMoveFast(CircularBuffer* pDest, CircularBuffer* pSource)
   // для копирования данных между буферами используйте фукцнию memcpy
   // желательно чтобы число вызовов memcpy не превышало кол-во непрерывных блоков данных, которые нужно скопировать
   // т.е чтобы число вызовов memcpy было минимально возможным.
+  
+  // 
+  if (pDest == pSource)
+  {
+    return 0;
+  }
+  
+  //
+  if (IsFull(pDest))
+    return 0;
+  
+  //
+  if (IsEmpty(pSource))
+    return 0;
 
+  size_t count = 0; // init
+  if ((GetDataCount(pSource)) < (BUFFER_SIZE - GetDataCount(pDest)))
+  {
+    //
+    count = GetDataCount(pSource);
+    memcpy(pDest->data+pDest->last, pSource->data+pSource->first, count);
+    ClearBuf(pSource);
+    pDest->last += count; 
+    return count;
+  }
+  else
+  {
+    //
+    count = (BUFFER_SIZE - 1) - GetDataCount(pDest);
+    memcpy(pDest->data+pDest->last, pSource->data+pSource->first, count);
+    pDest->last += count;
+    pSource->first += count;
+    return count;
+  }
+  
 }
 
 // вспомогательная функция для отладки
@@ -111,6 +182,7 @@ void PrintBuffer(CircularBuffer* pBuf)
   printf("\n");
 }
 
+
 // код ниже можно менять по своему усмотрению для тестирования фукнций
 
 CircularBuffer bufferA;
@@ -122,6 +194,8 @@ int main(){
 
         WriteByte(&bufferA, 4);
         WriteByte(&bufferA, 5);
+        WriteByte(&bufferA, 6);
+        WriteByte(&bufferA, 7);        
 
         WriteByte(&bufferB, 1);
         WriteByte(&bufferB, 2);
@@ -140,10 +214,277 @@ int main(){
         printf("BufferB after move:");
         PrintBuffer(&bufferB);
         
+        //WriteByte(&bufferB, 8);
+        printf("add 8: %s\n", WriteByte(&bufferB, 8) ? "true" : "false");
+
         ReadByte(&bufferB);
+        ReadByte(&bufferB); //
   
         printf("BufferB after read:");
         PrintBuffer(&bufferB);
-  
+
+        WriteByte(&bufferB, 1);
+
+        printf("BufferB after write:");
+        PrintBuffer(&bufferB);
+
+        printf("bufferB->first: %zu \n", bufferB.first);
+        printf("bufferB->last: %zu \n", bufferB.last);
+        printf("GetDataCount B: %zu \n", GetDataCount(&bufferB));
+        printf("last - first: %zu \n", (bufferB.last - bufferB.first) & (BUFFER_SIZE - 1));
+
+        printf("IsEmpty A: %s\n", IsEmpty(&bufferA) ? "true" : "false");
+        printf("IsEmpty B: %s\n", IsEmpty(&bufferB) ? "true" : "false");
+
+        printf("IsFull A: %s\n", IsFull(&bufferA) ? "true" : "false");
+        printf("IsFull B: %s\n", IsFull(&bufferB) ? "true" : "false");
+
+        printf("GetDataCount A: %zu\n", GetDataCount(&bufferA));
+        printf("GetDataCount B: %zu\n", GetDataCount(&bufferB));
+
+        //
+        TestCase_01(&bufferA, &bufferB);
+        TestCase_02(&bufferA, &bufferA);
+        TestCase_03(&bufferA, &bufferB);
+        TestCase_04(&bufferA, &bufferB);
+        TestCase_05(&bufferA, &bufferB);
+        TestCase_06(&bufferA, &bufferB);
+
   return 0;
+}
+
+void TestCase_01(CircularBuffer* pBufA, CircularBuffer* pBufB)
+{
+  printf("\n");
+  printf("%s - for BufMoveSlow()\n", __func__ );
+
+  ClearBuf(pBufA);
+  ClearBuf(pBufB);
+
+  WriteByte(pBufA, 4);
+  WriteByte(pBufA, 5);
+  WriteByte(pBufA, 6);
+  WriteByte(pBufA, 7);
+  WriteByte(pBufA, 8);
+
+  WriteByte(pBufB, 1);
+  WriteByte(pBufB, 2);
+  WriteByte(pBufB, 3);
+
+  printf("pBufA before move:");
+  PrintBuffer(pBufA);
+  printf("pBufB before move:");
+  PrintBuffer(pBufB);
+
+  size_t res = BufMoveSlow(pBufB, pBufA);
+  printf("BufMoveSlow moved %zu item(s) from pBufA to pBufB\n", res);
+
+  printf("pBufA after move:");
+  PrintBuffer(pBufA);
+  printf("pBufB after move:");
+  PrintBuffer(pBufB);
+}
+
+void TestCase_02(CircularBuffer* pBufA, CircularBuffer* pBufB)
+{ 
+  CircularBuffer* tmp = pBufB; 
+  printf("\n");
+  printf("%s - for BufMoveFast() where BuffA and BuffB has common address\n", __func__ );
+
+  ClearBuf(pBufA);
+  ClearBuf(pBufB);
+
+  WriteByte(pBufA, 4);
+  WriteByte(pBufA, 5);  
+
+  WriteByte(pBufB, 1);
+  WriteByte(pBufB, 2);
+  WriteByte(pBufB, 3);
+
+  printf("Before move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+  
+  printf("BufA addres:");
+  printf("%p\n", (void*)pBufA);
+  printf("BufB addres:");
+  printf("%p\n", (void*)pBufB);
+
+  pBufB = pBufA;
+   
+  printf("*** \n");
+  size_t res = BufMoveFast(pBufB, pBufA);
+  printf("BufMoveFast moved %zu item(s) from BufA to BufB\n", res);
+  printf("*** \n");
+
+  printf("After move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+
+  printf("BufA addres:");
+  printf("%p\n", (void*)pBufA);
+  printf("BufB addres:");
+  printf("%p\n", (void*)pBufB);
+
+  printf("Restore addresses:\n");
+  pBufB = tmp; // or use const params
+
+  printf("BufA addres:");
+  printf("%p\n", (void*) &pBufA);
+  printf("BufB addres:");
+  printf("%p\n", (void*) &pBufB);
+}
+
+void TestCase_03(CircularBuffer* pBufA, CircularBuffer* pBufB)
+{ 
+  printf("\n");
+  printf("%s - for BufMoveFast() where destination buffer full\n", __func__ );
+
+  ClearBuf(pBufA);
+  ClearBuf(pBufB);
+  
+  WriteByte(pBufA, 8);
+
+  WriteByte(pBufB, 1);
+  WriteByte(pBufB, 2);
+  WriteByte(pBufB, 3);
+  WriteByte(pBufB, 4);
+  WriteByte(pBufB, 5);
+  WriteByte(pBufB, 6);
+  WriteByte(pBufB, 7);
+
+  printf("Before move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+
+  printf("Is BuffA full: %s\n", IsFull(&bufferA) ? "true" : "false");
+  printf("Is BuffB full: %s\n", IsFull(&bufferB) ? "true" : "false");
+
+  printf("*** \n");
+  size_t res = BufMoveFast(pBufB, pBufA);
+  printf("BufMoveFast moved %zu item(s) from BufA to BufB\n", res);
+  printf("*** \n");
+
+  printf("After move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+
+  printf("Is BuffA full: %s\n", IsFull(&bufferA) ? "true" : "false");
+  printf("Is BuffB full: %s\n", IsFull(&bufferB) ? "true" : "false");
+}
+
+void TestCase_04(CircularBuffer* pBufA, CircularBuffer* pBufB)
+{ 
+  printf("\n");
+  printf("%s - for BufMoveFast() where source buffer empty\n", __func__ );
+
+  ClearBuf(pBufA);
+  ClearBuf(pBufB);
+  
+  WriteByte(pBufB, 1);
+  WriteByte(pBufB, 2);
+  WriteByte(pBufB, 3);
+
+  printf("Before move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+
+  printf("Is BuffA empty: %s\n", IsEmpty(&bufferA) ? "true" : "false");
+  printf("Is BuffB empty: %s\n", IsEmpty(&bufferB) ? "true" : "false");
+
+  printf("*** \n");
+  size_t res = BufMoveFast(pBufB, pBufA);
+  printf("BufMoveFast moved %zu item(s) from BufA to BufB\n", res);
+  printf("*** \n");
+
+  printf("After move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+
+  printf("Is BuffA empty: %s\n", IsEmpty(&bufferA) ? "true" : "false");
+  printf("Is BuffB empty: %s\n", IsEmpty(&bufferB) ? "true" : "false");
+}
+
+void TestCase_05(CircularBuffer* pBufA, CircularBuffer* pBufB)
+{ 
+  printf("\n");
+  printf("%s - for BufMoveFast() WHERE source buffer less than destination\n", __func__ );
+  printf(" AND source buffer not empty AND destination buffer not full\n");
+  
+  ClearBuf(pBufA);
+  ClearBuf(pBufB);
+  
+  WriteByte(pBufA, 1);
+  WriteByte(pBufA, 2);
+
+  WriteByte(pBufB, 3);
+  WriteByte(pBufB, 4);
+  WriteByte(pBufB, 5);
+  WriteByte(pBufB, 6);
+
+  printf("Before move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+
+  printf("*** \n");
+  size_t res = BufMoveFast(pBufB, pBufA);
+  printf("BufMoveFast moved %zu item(s) from BufA to BufB\n", res);
+  printf("*** \n");
+
+  printf("After move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+}
+
+void TestCase_06(CircularBuffer* pBufA, CircularBuffer* pBufB)
+{ 
+  printf("\n");
+  printf("%s - for BufMoveFast() WHERE source buffer more than destination\n", __func__ );
+  printf(" AND source buffer not empty AND destination buffer not full\n");
+  
+  ClearBuf(pBufA);
+  ClearBuf(pBufB);
+  
+  WriteByte(pBufA, 1);
+  WriteByte(pBufA, 2);
+  WriteByte(pBufA, 3);
+
+  WriteByte(pBufB, 4);
+  WriteByte(pBufB, 5);
+  WriteByte(pBufB, 6);
+  WriteByte(pBufB, 7);
+  WriteByte(pBufB, 8);
+
+  printf("Before move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
+
+  printf("*** \n");
+  size_t res = BufMoveFast(pBufB, pBufA);
+  printf("BufMoveFast moved %zu item(s) from BufA to BufB\n", res);
+  printf("*** \n");
+
+  printf("After move:\n");
+  printf("BufA:");
+  PrintBuffer(pBufA);
+  printf("BufB:");
+  PrintBuffer(pBufB);
 }
